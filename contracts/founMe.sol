6f81c2152bc91ca695bcb9cb04efcbb1962fd77b
月有此生 <807925743@qq.com>
2025-03-11 21:08:52 +0800

    project of construct_foundme inite

---
+++ contracts/founMe.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract foundMe {
+    //众筹合约
+    // 1. 创建一个收款函数
+    // 2. 记录投资人并且查看
+    // 3. 在锁定期内，达到目标值，生产商可以提款
+    // 4. 在锁定期内，没有达到目标值，投资人在锁定期以后退款
+    constructor() {}
+}
